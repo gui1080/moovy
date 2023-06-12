971cd11b1a622a4d9cdc88ff3de7ba3e956e9f81
Guilherme Braga <fbguib@gmail.com>
2023-06-12 18:33:07 -0300

    jwt token and users on database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { OmdbService } from './omdb/omdb.service'
 import { OmdbController } from './omdb/omdb.controller'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './users/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRootAsync(typeOrmConfig),
+      AuthModule
+    ],
   controllers: [OmdbController],
   providers: [OmdbService],
 })
